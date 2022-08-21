976846359d8560a7f470efe21d85438f80c1b1c6
OlgaBobryk <oljunja-86@mail.ru>
2022-08-21 12:53:32 +0300

    log4j.properties added CreateAccountTest.java changed AccountsPage changed LoginPage.java changed NewAccountModalPage.java changed NewAccountNamePage.java changed pom changed

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,11 +2,17 @@ package pages;
 
 import constants.Credentials;
 import constants.Urls;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import tests.CreateAccountTest;
 
 
 public class LoginPage extends BasePage {
+    private static final Logger LOGGER = LogManager.getLogger(LoginPage.class.getName());
+
+
     private By USERNAME_INPUT = By.id("username");
     private By PASSWORD_INPUT = By.id("password");
     private By LOGIN_BUTTON = By.id("Login");
@@ -17,9 +23,13 @@ public class LoginPage extends BasePage {
     }
 
     public void loginToSalesforce() {
+        LOGGER.debug(String.format("Attempt to open URL:%s", Urls.SALES_FORCE_LOGIN));
         driver.get(Urls.SALES_FORCE_LOGIN);
+        LOGGER.debug(String.format("Atempt to enter username:%s into the field find %s", Credentials.USERNAME, USERNAME_INPUT));
         driver.findElement(USERNAME_INPUT).sendKeys(Credentials.USERNAME);
+        LOGGER.debug(String.format("Attempt to enter password: %s into the field  find  %s", Credentials.PASSWORD, PASSWORD_INPUT));
         driver.findElement(PASSWORD_INPUT).sendKeys(Credentials.PASSWORD);
+        LOGGER.debug(String.format("Attempt to click Login button find %s", LOGIN_BUTTON));
         driver.findElement(LOGIN_BUTTON).click();
 
     }
--- src/test/java/tests/CreateAccountTest.java
+++ src/test/java/tests/CreateAccountTest.java
@@ -1,27 +1,42 @@
 package tests;
 
 import models.NewAccountModel;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AccountsPage;
 import pages.LoginPage;
+import pages.NewAccountModalPage;
 import pages.NewAccountNamePage;
 import testdata.PrepareNewAccountData;
 
 public class CreateAccountTest extends BaseTest {
+    private static final Logger LOGGER = LogManager.getLogger(CreateAccountTest.class.getName());
+
 
     @Test
     public void createAccount() {
         LoginPage loginPage = new LoginPage(driver);
+        LOGGER.info(String.format("Page %s initialized", LoginPage.class.getName()));
+        LOGGER.info(String.format("Login to SalesForce"));
         loginPage.loginToSalesforce();
         AccountsPage accountsPage = new AccountsPage(driver);
         NewAccountModel accountModel = PrepareNewAccountData.getValidDate();
-        accountsPage
-                .openAccountsPage()
-                .openNewAccountModal()
-                .fillInAccountForm(accountModel);
+        LOGGER.info(String.format("Page %s initialized",AccountsPage.class.getName() ));
+        LOGGER.info(String.format("Page %s initialized",NewAccountModel.class.getName() ));
+        LOGGER.info(String.format("Open  AccountsPage"));
+        accountsPage.openAccountsPage();
+        LOGGER.info(String.format("Open the New AccoutsPage"));
+        accountsPage.openNewAccountModal();
+        NewAccountModalPage newAccountModalPage= new NewAccountModalPage(driver);
+        LOGGER.info(String.format("Page %s initialized",NewAccountModalPage.class.getName() ));
+        LOGGER.info(String.format("Enter data in the Form and create New Account"));
+        newAccountModalPage.fillInAccountForm(accountModel);
         NewAccountNamePage accountName = new NewAccountNamePage(driver);
-       Assert.assertEquals(accountModel.getAccountName(), accountName.findAccountName(), "Accountnames are different");
+        LOGGER.info(String.format("Page %s initialized",NewAccountNamePage.class.getName() ));
+        LOGGER.info(String.format("Check if actual  Account Name  %s and expected Account Name %s are identical",accountModel.getAccountName(), accountName.findAccountName() ));
+        Assert.assertEquals(accountModel.getAccountName(), accountName.findAccountName(), "Accountnames are different");
 
     }
 }
